5d6c92aa12ab948c43725f52680b01c950419d60
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-15 16:08:59 +0100

    direct flights

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,20 +7,48 @@ import FlyToButton from './FlyToButton.jsx';
 const App = props => {
 
     const [outputValues, setOutputValues] = useState([]);
-    const [flyFrom, setFlyFrom] = useState('PRG');
-    const [flyTo, setFlyTo] = useState('MXP');
+    const [flyFrom, setFlyFrom] = useState('');
+    const [flyTo, setFlyTo] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [checked, setChecked] = useState(false);
+    const [maxStopovers, setMaxStopovers] = useState(10);
 
-    let URL = `https://api.skypicker.com/flights?&fly_from=${flyFrom}&fly_to=${flyTo}&partner=picky&limit=1&max_stopovers=1`;
+    let URL = `https://api.skypicker.com/flights?&fly_from=${flyFrom}&fly_to=${flyTo}&partner=picky&limit=10&max_stopovers=${maxStopovers}`;
 
      const changeFlyFrom = (flyFrom) => {
+
+        setLoading(false)
         setFlyFrom(flyFrom)
 
     }
 
     const changeFlyTo= (flyTo) => {
+
+        setLoading(false)
         setFlyTo(flyTo)
 
     }
+
+    const changeChecked = () => {
+
+        setChecked(!checked)
+        
+
+        if (checked){
+            setMaxStopovers(10)
+        }
+        else
+        {
+            setMaxStopovers(0)
+
+        } 
+
+    }
+
+    console.log(maxStopovers);
+    
+
+
     
     const callApi = (url) => {
 
@@ -29,6 +57,9 @@ const App = props => {
             fetch(url)
             .then(response => response.json())
             .then(data => {
+
+
+                setLoading(true)
                 
                 setOutputValues(data.data)
             })
@@ -39,15 +70,80 @@ const App = props => {
 
     callApi(URL);
 
-    return (
-        <div><h1>Fly happy!</h1> 
-            {outputValues.length ? outputValues.map((flightData) => {
+    if (loading && !outputValues.length)
+    {
+
+        return (
+
+            <div>
+                <h1>Fly happy!</h1> 
+                <h1>Pick a flight!</h1> 
+
+                <div className="selection">
+                    <div><FlyFromButton key={flyFrom} changeFlyFrom={changeFlyFrom} /></div>
+                    <div><FlyToButton key={flyTo} changeFlyTo={changeFlyTo}/></div>
+
+                    <label htmlFor="checkbox">Only direct flights</label>
+                    <input
+                        id="checkbox"
+                        type="checkbox"
+                        checked={checked}
+                        onChange={changeChecked}
+                    />
+                </div>
+            </div>
+
+        );
+    }
+    else if (!loading) {
+
+        return (
+
+            <div>
+                    <h1>Fly happy!</h1> 
+
+                    <h1>Loading!</h1> 
+
+                    <div className="selection">
+                        <div><FlyFromButton key={flyFrom} changeFlyFrom={changeFlyFrom} /></div>
+                        <div><FlyToButton key={flyTo} changeFlyTo={changeFlyTo}/></div>
+                        <label htmlFor="checkbox">Only direct flights</label>
+                            <input
+                                id="checkbox"
+                                type="checkbox"
+                                checked={checked}
+                                onChange={changeChecked}
+                            />
+                    </div>
+
+                </div>
+        );
+
+
+
+    }
+    else{
+
+        return (
+        <div>
+            <h1>Fly happy!</h1>
+            <div className="selection">
+                <div><FlyFromButton key={flyFrom} changeFlyFrom={changeFlyFrom} /></div>
+                <div><FlyToButton key={flyTo} changeFlyTo={changeFlyTo}/></div>
+                </div>
+
+                 <label htmlFor="checkbox">Only direct flights</label>
+                        <input
+                            id="checkbox"
+                            type="checkbox"
+                            checked={checked}
+                            onChange={changeChecked}
+                        /> 
+            {outputValues.map((flightData) => {
                 return (
                     <>
-                    <div className="selection">
-                     <div><FlyFromButton key={flyFrom} changeFlyFrom={changeFlyFrom} /></div>
-                     <div><FlyToButton key={flyTo} changeFlyTo={changeFlyTo}/></div>
-                     </div>
+                    
+                    
                     {flightData.route.map((route) => {
                         return (
                             <div className="flights">
@@ -69,10 +165,12 @@ const App = props => {
                     </>
                    
                 )
-            }) 
-        : 'Loading...'}      
+            })}      
         </div>
     );
+
+    }
+
 };
 
 export default App;
